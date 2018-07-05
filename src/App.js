7e4cfc4726691358924bd883d7f8080bce992fe1
Thomas <tai.okubo93@gmail.com>
2018-07-04 23:16:28 -0300

    Create a list of users
    It shows a hard coded list of users and it also has a button to add a static user

--- src/App.js
+++ src/App.js
@@ -13,7 +13,87 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <UserList/>
       </div>
+    );
+  }
+}
+
+class UserList extends Component {
+  
+  constructor (props) {
+    super(props);
+    this.state = {
+      users : [
+        {
+          name: 'Lucas',
+          lastName: 'Natingale',
+          trashCounter: 0
+        },
+        {
+          name: 'Marcelo',
+          lastName: 'Amarildo',
+          trashCounter: 0
+        },
+        {
+          name: 'Pericles',
+          lastName: 'Montana',
+          trashCounter: 0
+        },
+        {
+          name: 'Thiago',
+          lastName: 'Otake',
+          trashCounter: 0
+        },
+        {
+          name: 'Vinicius',
+          lastName: 'Neymar',
+          trashCounter: 0
+        }
+      ]}   
+      
+      this.addUser = this.addUser.bind(this);
+  }
+
+  addUser(e) {
+    var newUser = {
+      name: 'Menino',
+      lastName: 'Ney',
+      trashCounter: 0
+    }
+
+    this.setState({
+      users: this.state.users.concat([newUser])
+    });
+    
+  }
+
+  render() {
+    return(
+      <div>
+        <div>
+          {
+            this.state.users.map((user, i) => <User userData={user} key={i}/>)
+          }
+        </div>
+
+        <button onClick={this.addUser}>Add new user</button>
+      </div>
+    );
+  }
+}
+
+class User extends Component {
+  render() {
+
+    let user = this.props.userData;
+    var userBlock = {marginRight : '20px'}
+
+    return (
+      <div> 
+        <div> <span style={userBlock}>{user.name} {user.lastName}</span> </div>
+      </div> 
     );
   }
 }
